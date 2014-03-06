3a7177ce512477ef509f8a93e091e1f519f79847
dimitri <dvalax@ekito.fr>
2014-03-06 21:53:15 +0100

    config port

--- src/main/java/root/Application.java
+++ src/main/java/root/Application.java
@@ -4,10 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
+import root.controller.CityController;
+
 @ComponentScan
 @EnableAutoConfiguration
 public class Application {
-    public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+        String webPort = System.getenv("PORT");
+        if (webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+        System.setProperty("server.port", webPort);
         SpringApplication.run(Application.class, args);
     }
 }
