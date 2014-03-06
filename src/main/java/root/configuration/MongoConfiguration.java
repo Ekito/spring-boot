6e431a7647f6bd04b2de39e40bdf335951995205
dimitri <dvalax@ekito.fr>
2014-03-06 21:38:34 +0100

    config port

--- src/main/java/root/configuration/MongoConfiguration.java
+++ src/main/java/root/configuration/MongoConfiguration.java
@@ -1,22 +1,22 @@
-package root.configuration;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.MongoDbFactory;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
-
-import com.mongodb.MongoClient;
-
-
-@Configuration
-public class MongoConfiguration {
-  
-  public @Bean MongoDbFactory mongoDbFactory() throws Exception {
-    return new SimpleMongoDbFactory(new MongoClient(), "uspopulation");
-  }
-
-  public @Bean MongoTemplate mongoTemplate() throws Exception {
-    return new MongoTemplate(mongoDbFactory());
-  }
-}
+//package root.configuration;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.mongodb.MongoDbFactory;
+//import org.springframework.data.mongodb.core.MongoTemplate;
+//import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+//
+//import com.mongodb.MongoClient;
+//
+//
+//@Configuration
+//public class MongoConfiguration {
+//  
+//  public @Bean MongoDbFactory mongoDbFactory() throws Exception {
+//    return new SimpleMongoDbFactory(new MongoClient(), "uspopulation");
+//  }
+//
+//  public @Bean MongoTemplate mongoTemplate() throws Exception {
+//    return new MongoTemplate(mongoDbFactory());
+//  }
+//}
--- src/main/java/root/repository/CityRepositoryCustom.java
+++ src/main/java/root/repository/CityRepositoryCustom.java
@@ -1,11 +1,11 @@
-package root.repository;
-
-import java.util.List;
-
-import root.domain.City;
-
-public interface CityRepositoryCustom {
-	
-	public List<City> findCityLike(String name);
-
-}
+//package root.repository;
+//
+//import java.util.List;
+//
+//import root.domain.City;
+//
+//public interface CityRepositoryCustom {
+//	
+//	public List<City> findCityLike(String name);
+//
+//}
