919064595ebba1d61cc97d402f81042445256a68
dimitri <dvalax@ekito.fr>
2014-03-06 21:50:21 +0100

    config port

--- src/main/java/root/controller/CityController.java
+++ src/main/java/root/controller/CityController.java
@@ -1,5 +1,6 @@
 package root.controller;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,15 @@ public class CityController {
 		
 		return "yes!!!";
 	}
+	
+	public static void main(String[] args) throws Exception {
+        String webPort = System.getenv("PORT");
+        if (webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+        System.setProperty("server.port", webPort);
+        SpringApplication.run(CityController.class, args);
+    }
 
 
 }
